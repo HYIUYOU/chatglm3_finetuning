8c947cf7dde058b563cf10769f926fdb746c5850
HYIUYOU <52117880+HYIUYOU@users.noreply.github.com>
2024-04-09 16:45:13 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -11,8 +11,8 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed --master_port 520 train.py \
-    --train_path /root/heyiyuan/project/ChatGLM3/dataset/dataset.json \
-    --model_name_or_path /root/heyiyuan/project/ChatGLM3/chatglm3-6b/ \
+    --train_path your_dataset_path/dataset.json \
+    --model_name_or_path chatglm3-6b_path/ \
     --per_device_train_batch_size 1 \
     --max_len 512 \
     --max_src_len 256 \
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed --master_port 520 train.py \
     --train_type freeze \
     --freeze_module_name "layers.27.,layers.26.,layers.25.,layers.24." \
     --seed 1234 \
-    --ds_file /root/heyiyuan/project/ChatGLM3/ChatGLM-Finetuning/ds_zero3_no_offload.json \
+    --ds_file ds_zero3_no_offload.json \
     --gradient_checkpointing \
     --show_loss_step 10 \
     --output_dir $OUTPUT \
